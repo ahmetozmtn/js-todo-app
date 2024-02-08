3ddc5ce9838035200d3dcdcfc26bfb594ddbb72a
ahmetozmtn <ahmethack13@gmail.com>
2024-02-08 12:52:51 +0300

    feat: onclick function added

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+let list = document.querySelector("ul.list");
+let btnAdd = document.getElementById("btnAdd");
+let listTask = [];
+let taskInput = document.getElementById("enterTask");
+
+if (localStorage.getItem("listTask") != null) {
+    listTask = JSON.parse(localStorage.getItem("listTask"));
+}
+
+btnAdd.onclick = function (event) {
+    event.preventDefault();
+    let content = document.getElementById("enterTask").value;
+    if (content != "") {
+        listTask.unshift({
+            content: content,
+            status: "doing",
+        });
+    }
+    addTask();
+    document.getElementById("enterTask").value = "";
+    saveLocalStorage();
+};
